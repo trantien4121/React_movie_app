2c9ccd2146c1d1eac5da92db25bc9aea05332b60
TranTien4121 <trantien04012k1@gmail.com>
2024-05-30 20:27:20 +0700

    new up

---
+++ src/components/Login.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import Input from "./form/Input";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { setJwtToken } = useOutletContext();
+    const { setAlertClassName } = useOutletContext();
+    const { setAlertMessage } = useOutletContext();
+
+    const { toggleRefresh } = useOutletContext();
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("email/password", email, password);
+
+        // build the request payload
+        let payload = {
+            email: email,
+            password: password
+        }
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(payload)
+        }
+
+        fetch(`${process.env.REACT_APP_BACKEND}/authenticate`, requestOptions)
+            .then(response => response.json())
+            .then((data) => {
+                if (data.error){
+                    setAlertClassName("alert-danger");
+                    setAlertMessage(data.message);
+                } else {
+                    setJwtToken(data.access_token);
+                    setAlertClassName("d-none");
+                    setAlertMessage("");
+                    toggleRefresh(true);
+                    navigate("/");
+                }
+            })
+            .catch(error => {
+                setAlertClassName("alert-danger");
+                setAlertMessage(error);
+            })
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <hr />
+
+            <form onSubmit={handleSubmit}>
+                <Input
+                    title="Email Address"
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    autoComplete="email-new"
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+
+                <Input
+                    title="Password"
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    autoComplete="password-new"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+
+                <hr />
+
+                <input 
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
+                />
+            </form>
+        </div>
+    );
+}
+
+export default Login;
